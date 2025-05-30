c4e463fd4307c049d56ab175e8d35de4ae6b9ed5
jfmejia-q <fernanmej1416@gmail.com>
2025-05-30 16:22:31 -0500

    Add files via upload

--- src/Ejemplojava.java
+++ src/Ejemplojava.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 
-public class Ejercicio {
+public class Ejemplojava {
     public static void main(String[] args) {
         Scanner input = new Scanner (System.in);
+        System.out.println("hola mundo");
         System.out.print("Imgrese su nombre: ");
         String nombre = input.nextLine();
         System.out.print("Ingrese su edad: ");
         int edad = input. nextInt();
-        System.out.print("Ingrese su altura: ");
-        
 
-        System.out.println("hola, " + nombre + ", Tienes" + edad + " años, " + " altra: " );
+        System.out.println("hola, " + nombre + ", Tienes" + edad + " años. ");
+
+        System.out.println("Chupame las bolas");
+        
     }
 }
--- src/Ejercicio7.java
+++ src/Ejercicio7.java
@@ -1,28 +1,27 @@
-
 import java.util.Scanner;
+
 public class Ejercicio7 {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int numero;
-        System.out.println("ingrese un numero entero");
-        numero= entrada.nextInt();
-        if (numero<10 && numero>0) {
-            System.out.println("el numero es de una cifra");
-            
-        }else if (numero>=10 && numero<100) {
-            System.out.println("el numero tiene dos cifras");
-            
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("digita un numero hasta de 3 cifras");
+        double numero = scanner.nextInt();
+        if (numero > 0 || numero < 10) {
+            System.out.println("tiene 1 cifra");
         } 
-
-        if (numero>=100 && numero<1000) {
-            System.out.println("el numero tiene tres cifras");
+        else if (numero>10 || numero<100) {
+            System.out.println("Tiene 2 cifras");
             
-        }else if (numero>=1000) {
-            
-        
-            System.out.println("el numero de cifras es mayor");
         }
+        else if (numero>0 || numero <1000) {
+            System.out.println("tiene 3 cifras");
+            
+        }
+        else if (numero>0 || numero<10000) {
+            System.out.println("Escriba numero de 3 cifras.");
 
+        }
+         
+        
+        
     }
-    
 }
